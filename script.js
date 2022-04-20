0acda1a1c75fd8665948e0998923f7e9c6fd169f
Jeremiah <jeremiahlat@gmail.com>
2022-04-20 04:06:25 +0100

    Fixed a JavaScript Issue

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ navBar.addEventListener("click", function () {
 const date = (document.getElementById("date").innerHTML =
   new Date().getFullYear());
 
-// // ********** nav toggle ************
-// // select button and links
-// const navBtn = document.getElementById("nav-toggle");
-// const links = document.getElementById("nav-links");
-// // add event listener
-// navBtn.addEventListener("click", () => {
-//   links.classList.toggle("show-links");
-// });
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
 
 // ********** smooth scroll ************
 // select links
